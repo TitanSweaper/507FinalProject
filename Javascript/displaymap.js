cfcc88adb20afd5261d97326cd0c75619d6f6c9f
TitanSweaper <jacob.shay@gmail.com>
2019-04-24 18:14:16 -0500

    oops

--- Javascript/displaymap.js
+++ Javascript/displaymap.js
@@ -2,6 +2,6 @@
 const images = ['one','two','three','four','five','six']
 function display() {
    for(let counter = 0; counter < images.length; counter++)
-	document.getElementById(images[counter])..style.height = "0px";
+	document.getElementById(images[counter]).style.height = "0px";
 }
 display();
