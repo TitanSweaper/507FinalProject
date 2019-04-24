0962de9854cf2dd749439553d942a586369b1313
TitanSweaper <jacob.shay@gmail.com>
2019-04-24 18:13:59 -0500

    javainitialize

--- Javascript/displaymap.js
+++ Javascript/displaymap.js
@@ -1,6 +1,7 @@
 // JavaScript Document
-
+const images = ['one','two','three','four','five','six']
 function display() {
-   ("Hello World!");
+   for(let counter = 0; counter < images.length; counter++)
+	document.getElementById(images[counter])..style.height = "0px";
 }
 display();
