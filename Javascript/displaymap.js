1285d4084650b3c8eb885369dc2f724348ef9202
TitanSweaper <jacob.shay@gmail.com>
2019-04-25 18:13:44 -0500

    call javascript initial

--- Javascript/displaymap.js
+++ Javascript/displaymap.js
@@ -3,4 +3,5 @@ const images = ['one','two','three','four','five','six']
 function display() {
    for(let counter = 0; counter < images.length; counter++)
 	document.getElementById(images[counter]).style.height = "0px";
+	//document.getElementById("myBtn").style.height = "50px";
 }
