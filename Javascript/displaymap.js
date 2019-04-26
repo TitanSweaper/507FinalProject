bb0f833a7e113b23003984d92ffac0d22a96e4b3
TitanSweaper <jacob.shay@gmail.com>
2019-04-26 00:08:15 -0500

    attempt to load one state at a time.

--- Javascript/displaymap.js
+++ Javascript/displaymap.js
@@ -3,5 +3,9 @@ const images = ['one','two','three','four','five','six']
 function display() {
    for(let counter = 0; counter < images.length; counter++)
 	document.getElementById(images[counter]).style.height = "0px";
-	//document.getElementById("myBtn").style.height = "50px";
+}
+
+function diplim(btname,state) {
+	display();
+	document.getElementById(state+btname).style.height = "auto";
 }
