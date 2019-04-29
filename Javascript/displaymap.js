1dbd7ce61376ba0c27ff1892528ba276c59d9262
TitanSweaper <jacob.shay@gmail.com>
2019-04-29 16:39:32 -0500

    test

--- Javascript/displaymap.js
+++ Javascript/displaymap.js
@@ -4,6 +4,14 @@ function display() {
    for(let counter = 0; counter < images.length; counter++)
 	document.getElementById(images[counter]).style.height = "0px";
 }
+function show(state) {
+	<div class="btn-group" role="group">
+	  <button type="button" class="btn btn-secondary" onClick="diplime(All,state)">All Mine Types</button>
+	  <button type="button" class="btn btn-secondary" onClick="diplime(mineral,state)">Mineral Mines</button>
+	  <button type="button" class="btn btn-secondary" onClick="diplime(SG,state)">Sand and Gravel</button>
+	  </div>
+}
+
 
 /*function diplim(btname,state) {
 	display();
