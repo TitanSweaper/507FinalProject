8fbfde9dbaa8857e46bb6d3a864f58282cf07506
TitanSweaper <jacob.shay@gmail.com>
2019-04-29 20:12:17 -0500

    Functioning buttons

--- Javascript/displaymap.js
+++ Javascript/displaymap.js
@@ -1,20 +1,28 @@
 // JavaScript Document
-const images = ['one','two','three','four','five','six']
-function display() {
-   for(let counter = 0; counter < images.length; counter++)
-	document.getElementById(images[counter]).style.height = "0px";
+//var state = document.getElementById('state').onload;
+//state = document.getElementById('state').onchange;
+window.onload = function show(state) {
+	if(state == 'california'){
+		document.getElementById("one").style.height="auto"
+	}else if(state == 'montana'){
+		document.getElementById("four").style.height="auto"
+	}
 }
-function show(state) {
-	document.getElementById('statebutton').innerHTML('<div class="btn-group" role="group">
-	  <button type="button" class="btn btn-secondary" onClick="diplime(All,state)">All Mine Types</button>
-	  <button type="button" class="btn btn-secondary" onClick="diplime(mineral,state)">Mineral Mines</button>
-	  <button type="button" class="btn btn-secondary" onClick="diplime(SG,state)">Sand and Gravel</button>
-	  </div>');
+//show(state);
+
+function show() {
+  var x = document.getElementById("state").value;
+  document.getElementById("demo").innerHTML = "You selected: " + x;
+  return x;
 }
 
+function change(nam) {
+	var placeholder = nam;
+	document.getElementById("test").innerHTML = "You selected: " + nam;
+	running(placeholder);
+}
 
-/*function diplim(btname,state) {
-	display();
-	const name = state + btname;
-	document.getElementById(name).style.height = "auto";
-}*/
+function running(change) {
+	var showe = show;
+	document.getElementById("check").innerHTML = "You selected: " + showe() + change;
+}
