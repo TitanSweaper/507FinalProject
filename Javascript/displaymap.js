4857e7bf90df5dfb7d08c0b121940b35658758e2
TitanSweaper <jacob.shay@gmail.com>
2019-05-06 15:30:11 -0500

    button shift

--- Javascript/displaymap.js
+++ Javascript/displaymap.js
@@ -2,7 +2,7 @@
 
 function set() {
 	//if(trigger == 0){}
-	document.getElementById("demo").innerHTML= '<br>';	
+	//document.getElementById("demo").innerHTML= '<br>';	
 	var loca = document.getElementById("state").value;
 	var src = document.getElementById(loca).cells[2].innerHTML;
 	document.getElementById("cat").src= src;
@@ -11,7 +11,7 @@ function setmineral(){
 	//document.getElementById("demo").innerHTML= "Minerals maps are currently unavailable."
 	//set(1);
 	
-	document.getElementById("demo").innerHTML= '<br>';
+	//document.getElementById("demo").innerHTML= '<br>';
 	var loca = document.getElementById("state").value;
 	var src = document.getElementById(loca).cells[3].innerHTML;
 	document.getElementById("cat").src= src;
@@ -21,7 +21,7 @@ function setSG(){
 	//document.getElementById("demo").innerHTML= "Sand and gavel maps are currently unavailable."
 	//set(1);
 	
-	document.getElementById("demo").innerHTML= '<br>';
+	//document.getElementById("demo").innerHTML= '<br>';
 	var loca = document.getElementById("state").value;
 	var src = document.getElementById(loca).cells[4].innerHTML;
 	document.getElementById("cat").src= src;
